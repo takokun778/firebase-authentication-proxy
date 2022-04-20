ae0186e6a2dc4e83ebe44abbc067a585f331bd93
takokun778 <takokun.778@gmail.com>
2022-04-20 23:03:11 +0900

    feat graceful shutdown

--- backend/driver/server/server.go
+++ backend/driver/server/server.go
@@ -7,14 +7,15 @@ import (
 	"github.com/takokun778/firebase-authentication-proxy/driver/injector"
 )
 
-type Server struct{}
-
-func Serve(port string) error {
+func Initialize(port string) *http.Server {
 	mux := http.NewServeMux()
 
 	router := NewRouter(injector.NewInjector())
 
 	mux.Handle("/", Middleware(router.Handle()))
 
-	return http.ListenAndServe(fmt.Sprintf(":%s", port), mux)
+	return &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: mux,
+	}
 }
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	_ "github.com/takokun778/firebase-authentication-proxy/driver/env"
 	"github.com/takokun778/firebase-authentication-proxy/driver/server"
@@ -10,5 +14,28 @@ import (
 
 func main() {
 	log.Printf("%s server starting...\n", os.Getenv("ENV"))
-	log.Fatal(server.Serve(os.Getenv("PORT")))
+
+	srv := server.Initialize(os.Getenv("PORT"))
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalln("Server closed with error: ", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGTERM, os.Interrupt)
+
+	log.Printf("SIGNAL %d received, then shutting down...\n", <-quit)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Failed to gracefully shutdown:", err)
+	}
+
+	log.Println("Server shutdown")
 }
