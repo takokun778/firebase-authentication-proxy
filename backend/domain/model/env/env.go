e39041d10eb5d8f4fffd3679db5446a88cd71233
takokun778 <takokun.778@gmail.com>
2022-05-04 10:27:03 +0900

    fix lint

--- backend/domain/model/env/env.go
+++ backend/domain/model/env/env.go
@@ -12,9 +12,7 @@ const (
 )
 
 func init() {
-	env := os.Getenv("ENV")
-
-	if env == dev {
+	if os.Getenv("ENV") == dev {
 		err := godotenv.Load(".env")
 		if err != nil {
 			log.Fatalf("error loading .env file %v", err)
