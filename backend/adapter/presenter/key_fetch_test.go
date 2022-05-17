b3b3f948cf47c2c456a7ccb79d8d5d51837cd0ad
takokun778 <takokun.778@gmail.com>
2022-05-17 22:44:00 +0900

    test add key fetch presenter

---
+++ backend/adapter/presenter/key_fetch_test.go
@@ -0,0 +1,51 @@
+package presenter_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/takokun778/firebase-authentication-proxy/adapter"
+	"github.com/takokun778/firebase-authentication-proxy/adapter/mock"
+	"github.com/takokun778/firebase-authentication-proxy/adapter/presenter"
+	"github.com/takokun778/firebase-authentication-proxy/usecase/port"
+)
+
+func TestKeyFetchPresenterRender(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		ctx    context.Context
+		output port.KeyFetchOutput
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "正常に動作することを確認",
+			args: args{
+				ctx: adapter.SetResWriter(context.Background(), &mock.ResponseWriter{
+					Assert: func(statusCode int) {
+						if statusCode != http.StatusOK {
+							t.Errorf("http status code: %d, expected %d", statusCode, http.StatusOK)
+						}
+					},
+				}),
+				output: port.KeyFetchOutput{
+					PublicKey: []byte{},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			p := presenter.NewKeyFetchPresenter()
+			p.Render(tt.args.ctx, tt.args.output)
+		})
+	}
+}
